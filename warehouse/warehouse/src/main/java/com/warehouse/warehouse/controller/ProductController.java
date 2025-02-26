9423dfc10db7daccb03119a5c8f9ab3f07e8f6e4
nipo20 <85899495+nipo20@users.noreply.github.com>
2025-02-26 10:12:49 +0100

    Update ProductController.java
    Added comments section

--- warehouse/warehouse/src/main/java/com/warehouse/warehouse/controller/ProductController.java
+++ warehouse/warehouse/src/main/java/com/warehouse/warehouse/controller/ProductController.java
@@ -18,29 +18,34 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    //Retrieve a product detail using ID
     @GetMapping("/product/{product_Id}")
     public ResponseEntity<Products> getProduct(@PathVariable("product_Id") Long product_Id) {
         Products getProductdetails = (Products) productService.getProduct(product_Id);
         return new ResponseEntity<>(getProductdetails, HttpStatus.OK);
     }
 
+    //Retrieve all products
     @GetMapping("/products")
     public List<Products> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    //Add product to the list
     @PostMapping("/product")
     public ResponseEntity<Products> addProduct(@RequestBody ProductData productData) {
         Products addedProducts = productService.addProduct(productData);
         return new ResponseEntity<>(addedProducts, HttpStatus.CREATED);
     }
 
+    //Update product using ID 
     @PutMapping("/product/{product_Id}")
     public ResponseEntity<Products> updateProduct(@PathVariable Long product_Id, @RequestBody Products updatedProduct) {
         Products updatedProd = productService.updateProduct(product_Id, updatedProduct);
         return new ResponseEntity<>(updatedProd, HttpStatus.OK);
     }
 
+    //Delete a product using ID 
     @DeleteMapping("/product")
     public ResponseEntity<Products> deleteProduct(@RequestParam(name = "product_Id") Long product_Id) {
         Products deleteProductById = productService.deleteProduct(product_Id);
